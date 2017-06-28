f38ee5a8881595dfdc9fcbc352c568e9ff894224
zhangtao <754710007@qq.com>
2017-06-28 14:40:41 +0800

    Youhua

---
+++ protected/modules/api/controllers/WxController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 问答提交的api
+ * @author weibaqiu
+ * @date 2015-11-10
+ */
+class WxController extends ApiController
+{
+	public function actionCheck()
+	{
+		if($_GET['echostr'])
+			echo json_encode(['echostr'=>$_GET['echostr']]);
+	}
+}
