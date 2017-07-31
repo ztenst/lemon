f8df5c784d914ba0ad0a210e63473738d2dbbf42
zhangtao <754710007@qq.com>
2017-07-31 13:41:42 +0800

    优化

--- themes/v2/views/home/news/info.php
+++ themes/v2/views/home/news/info.php
@@ -11,7 +11,7 @@
                     <table>
                         <tbody>
                             <tr class="firstRow">
-                                <td valign="top" width="569"><img src="<?=ImageTools::fixImage($info->image)?>" title="undefined" alt="undefined" /></td>
+                                <td valign="top" width="569"><img src="<?=ImageTools::fixImage($info->image)?>" /></td>
                                 <td style="word-break: break-all;" valign="top" width="569">
                                     
                                     <p><?=$info->content?></p>
